1de2e64bea6453c1b48e20523b564d24731f5e99
cesarionto <cesariopereiraneto@gmail.com>
2020-08-27 17:14:42 -0300

    Adicionando estado e comportamento no componente Login

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,6 +4,16 @@ import Card from '../Card';
 import FormGroup from '../FormGroup';
 
 class Login extends React.Component {
+
+    state = {
+        email : '',
+        senha : ''
+    }
+
+    entrar = () =>{
+        console.log('Email: ', this.state.email, 'Senha: ', this.state.senha)
+    }
+
     render(){
         return (
             <div className="container">
@@ -16,15 +26,16 @@ class Login extends React.Component {
                                         <div className="bs-component">
                                             <fieldset>
                                                 <FormGroup label="Email: *" htmlFor="exempleInputEmail">
-                                                    <input type="email" className="form-control"
+                                                    <input type="email" value= {this.state.email} onChange={e =>this.setState({email: e.target.value})}className="form-control"
                                                     id="exempleInputEmail" aria-describedby="emailHelp" placeholder="Digite o Email"/>
                                                 </FormGroup>
                                                 <FormGroup label="Senha: *" htmlFor="exempleInputSenha">
-                                                    <input type="password" className="form-control"
+                                                    <input type="password" value= {this.state.senha} onChange={e =>this.setState({senha: e.target.value})} className="form-control"
                                                     id="exempleInputSenha" placeholder="Password"/>
                                                 </FormGroup>
                                             </fieldset>
-                                        </div>
+                                            <button className="btn btn-success" onClick ={this.entrar}>Entrar</button>
+                                            <button className="btn btn-danger">Cadastrar</button>                                        </div>
                                     </div>
                                 </div>
                             </Card>
